4a3ccf55c5f553131167bd73cff9644d29093e6e
Olga Filatova <olyafilatov@gmail.com>
2019-09-22 20:12:03 +0300

    Testing component's DOM Binding

---
+++ src/app/components-dom/binding.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'app-binding',
+	template: '<h1>{{title}}</h1>',
+	styles: ['h1 { color: green; font-size: 350% }']
+})
+export class BindingComponent {
+	title = 'Test title';
+}
