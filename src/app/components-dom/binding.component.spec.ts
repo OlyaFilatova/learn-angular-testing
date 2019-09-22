4a16153e9e8b217d52aa95c0e5a67599a047108b
Olga Filatova <olyafilatov@gmail.com>
2019-09-22 20:13:49 +0300

    resolved lint errors

--- src/app/components-dom/binding.component.spec.ts
+++ src/app/components-dom/binding.component.spec.ts
@@ -2,33 +2,33 @@ import { TestBed, ComponentFixture } from '@angular/core/testing';
 import { BindingComponent } from './binding.component';
 
 describe('Test component binding', () => {
-	let component: BindingComponent;
-	let fixture: ComponentFixture<BindingComponent>
-	let h1: HTMLElement;
+  let component: BindingComponent;
+  let fixture: ComponentFixture<BindingComponent>;
+  let h1: HTMLElement;
 
-	beforeEach(() => {
-		TestBed.configureTestingModule({
-			declarations: [ BindingComponent ]
-		});
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [ BindingComponent ]
+    });
 
-		fixture = TestBed.createComponent(BindingComponent);
-		component = fixture.componentInstance;
-		h1 = fixture.nativeElement.querySelector('h1');
-	});
+    fixture = TestBed.createComponent(BindingComponent);
+    component = fixture.componentInstance;
+    h1 = fixture.nativeElement.querySelector('h1');
+  });
 
-	it('should not display title in the DOM after createComponent()', () => {
-		expect(h1.textContent).toEqual('');
-	});
+  it('should not display title in the DOM after createComponent()', () => {
+    expect(h1.textContent).toEqual('');
+  });
 
-	it('should display original title aflter detectChanges()', () => {
-		fixture.detectChanges();
-		expect(h1.textContent).toEqual(component.title);
-	});
+  it('should display original title aflter detectChanges()', () => {
+    fixture.detectChanges();
+    expect(h1.textContent).toEqual(component.title);
+  });
 
-	it('should display a different test title', () => {
-		component.title = 'New title';
-		fixture.detectChanges();
-		expect(h1.textContent).toEqual('New title');
-	});
+  it('should display a different test title', () => {
+    component.title = 'New title';
+    fixture.detectChanges();
+    expect(h1.textContent).toEqual('New title');
+  });
 
 });
