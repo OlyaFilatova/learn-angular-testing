c52360b01c2d224fb540bcc1b79a0a757152d269
Olga Filatova <olyafilatov@gmail.com>
2019-09-22 19:20:51 +0300

    Testing component's DOM

---
+++ src/app/components-dom/banner.component.spec.ts
@@ -0,0 +1,27 @@
+import { TestBed, ComponentFixture } from '@angular/core/testing';
+import { BannerComponent } from './banner.component';
+
+describe('Test component\'s DOM', () => {
+  let component: BannerComponent;
+  let fixture: ComponentFixture<BannerComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [ BannerComponent ]
+    });
+    fixture = TestBed.createComponent(BannerComponent);
+    component = fixture.componentInstance;
+  });
+  it('should be defined', () => {
+    expect(component).toBeDefined();
+  });
+  it('should contain "banner works!"', () => {
+    const bannerElement: HTMLElement = fixture.nativeElement;
+    expect(bannerElement.textContent).toContain('banner works!');
+  });
+  it('should have <p> with "banner works!"', () => {
+    const bannerElement: HTMLElement = fixture.nativeElement;
+    const p = bannerElement.querySelector('p');
+    expect(p.textContent).toEqual('banner works!');
+  });
+});
